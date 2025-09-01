078c5289f2aec3877e96be43ce28957b97a8939a
akshita03par <akshitaparmar9009@gmail.com>
2025-09-01 15:09:30 +0530

    add constants

--- my-project/src/components/Skills/Skills.jsx
+++ my-project/src/components/Skills/Skills.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React from "react";
+//import { SkillsInfo } from "../../constants.js";
 
-const Skills = ()=>{
-    return (
-        <div>
-
-        </div>
-    )
-}
+const Skills = () => {
+  return (
+    <section
+      id="skills"
+      className="py-24 pb-2 px-[12vw] md:px-[7vw] lg:px-[20vw] font-sans bg-skills-gradient "
+    >
+      {/* Section Title */}
+      <div className="text-center mb-8">
+        <h2 className="text-3xl sm:text-4xl font-bold text-white">SKILLLS</h2>
+      </div>
+    </section>
+  );
+};
 
 export default Skills;
